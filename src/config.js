668a1d7feb9ff9181608ec2a35b1b40f897ae800
Rob Ashton <robashton@codeofrob.com>
2012-07-07 15:14:19 +0200

    Added a different app for prod

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ var config = {
   },
   'prod': {
     fbcallback: 'http://wedrawthings.com/auth/facebook/callback',
-    fbclientid: '317150545045671',
-    fbclientsecret: 'b5290692796e002d8a442bf346e810db',
+    fbclientid: '174303766035042',
+    fbclientsecret: 'a6bea18ad44248eb94a348471eccc729',
     port: 8004,
     secret: '23g24ngk2mtg23ntgi23ngk2gn23g22'
   }
