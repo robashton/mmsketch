e803a49b788f6da290cfc5c9fe6e07bddb98e7bb
Rob Ashton <robashton@codeofrob.com>
2012-08-05 16:23:49 -0400

    Renabled replay

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 cat client/eventable.js client/statusdisplay.js client/feedbackdisplay.js client/feedbacktabs.js client/playerlistdisplay.js client/sharing.js client/game.js client/input.js client/canvas.js client/artpad.js client/artpadinput.js client/timer.js client/personalstatus.js client/app.js > client/raw.js
 
+cat client/eventable.js client/statusdisplay.js client/feedbackdisplay.js client/feedbacktabs.js client/playerlistdisplay.js client/sharing.js client/game.js client/input.js client/canvas.js client/artpad.js client/artpadinput.js client/timer.js client/personalstatus.js client/replaygame.js client/replayapp.js > client/rawreplay.js
+
 uglifyjs client/raw.js > site/game.js
+uglifyjs client/rawreplay.js > site/replay.js
